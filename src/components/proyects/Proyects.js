d6012ec8ee47315b322f94417c0eec9cb875d9f3
Marcobc-exe <marco.developing@gmail.com>
2022-11-21 21:06:19 -0300

    [Feat](Proyects): Github proyect links added, project image and stylling with css

--- src/components/proyects/Proyects.js
+++ src/components/proyects/Proyects.js
@@ -1,5 +1,6 @@
 import './proyects.css'
-import gmail from "../../img/gmail.png";
+import datasEmployee from "../../img/welcome.png";
+import userList from "../../img/preview-webpage.png"
 
 const Proyects = () => {
   return (
@@ -9,13 +10,21 @@ const Proyects = () => {
         <section className='container-flex'>
 
           <section className='container-element'>
-            <img src={gmail} alt="" className="imagen-gmail" />
-            <p>Description proyect</p>
+            <a href="https://github.com/Marcobc-exe/datas-employee#getting-started">
+              <img src={datasEmployee} alt="" className="project-imagen" />
+            </a>
+            <h4>Datas Employee</h4>
+            <p className='description-proyect'>Datas-employee is a Rest API developed using Node.js and Express where different types of queries to can be made to a database such as create, update, get an delete the data of the 3 employees registered in the db.
+
+            </p>
           </section>
 
           <section className='container-element'>
-            <img src={gmail} alt="" className="imagen-gmail" />
-            <p>Description proyect</p>
+            <a href="https://github.com/Marcobc-exe/Listando-usuarios#fetch-api-false">
+              <img src={userList} alt="" className="project-imagen" />
+            </a>
+            <h4>User List</h4>
+            <p className='description-proyect'>This is a simple web page where I make use a false api with user datas</p>
           </section>
 
         </section>
